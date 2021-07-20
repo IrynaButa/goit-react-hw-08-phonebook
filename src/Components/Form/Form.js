8091d93ddc705830d1cc3e5f1ce739c8b25d277c
Iryna Dagli <dagliirina@ukr.net>
2021-07-20 19:03:18 +0300

    start

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import shortid from "shortid";
+import styles from "./Form.module.css";
+import { connect } from 'react-redux';
+//import * as actions from '../../redux/contacts-actions';
+import  * as contactsOperations from '../../redux/contacts-operations';
+import * as contactsSelectors from '../../redux/contacts-selectors';
+//import IconButton from '../IconButton/IconButton';
+import { ReactComponent as Add } from "../icons/add.svg";
+
+class Form extends Component {
+  state = {
+    name: "",
+    number: "",
+  };
+
+  nameInputId = shortid.generate();
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    this.props.onAddContact(this.state);
+
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: "", number: "" });
+  };
+
+  render() {
+    return (
+      <form className={styles.form} onSubmit={this.handleSubmit}>
+        <label htmlFor={this.nameInputId}>
+          Name
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Phone number
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            required
+            value={this.state.number}
+            onChange={this.handleChange}
+          />
+        </label>
+
+        <button type="submit">
+          {" "}
+          <Add width="50" height="20" /> Add contact
+        </button>
+      </form>
+    );
+  }
+}
+Form.propTypes = {
+  onAddContact: PropTypes.func.isRequired,
+  items: PropTypes.arrayOf(PropTypes.any).isRequired,
+};
+
+const mapStateToProps = state => ({
+  items: contactsSelectors.getAllContacts(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  onAddContact: ({ name, number }) =>
+    dispatch(
+      contactsOperations.addContact({
+        name,
+        number,
+      }),
+    ),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Form);
---
+++ src/redux/contacts-reducer.js
@@ -0,0 +1,67 @@
+import { combineReducers } from 'redux';
+import { createReducer } from '@reduxjs/toolkit';
+
+//import { changeFilter, addContact, deleteContact } from './contacts-actions';
+
+import {
+  addContactRequest,
+  addContactSuccess,
+  addContactError,
+  deleteContactRequest,
+  deleteContactSuccess,
+  deleteContactError,
+  changeFilter,
+  fetchContactsRequest,
+  fetchContactsSuccess,
+  fetchContactsError,
+} from './contacts-actions';
+
+const itemsReducer = createReducer([], {
+  [fetchContactsSuccess]: (_, { payload }) => payload,
+  [addContactSuccess]: (state, { payload }) => [...state, payload],
+  [deleteContactSuccess]: (state, { payload }) =>
+    state.filter(({ id }) => id !== payload),
+});
+
+const loadingReducer = createReducer(false, {
+  [fetchContactsRequest]: () => true,
+  [fetchContactsSuccess]: () => false,
+  [fetchContactsError]: () => false,
+  [addContactRequest]: () => true,
+  [addContactSuccess]: () => false,
+  [addContactError]: () => false,
+  [deleteContactRequest]: () => true,
+  [deleteContactSuccess]: () => false,
+  [deleteContactError]: () => false,
+ 
+});
+
+const filterReducer = createReducer('', {
+  [changeFilter]: (_, { payload }) => payload,
+});
+
+//const error = createReducer(null, {});
+
+const contactsReducer = combineReducers({
+  items: itemsReducer,
+  filter: filterReducer,
+  loading: loadingReducer,
+});
+
+
+// const itemsReducer = createReducer([], {
+//   [addContact]: (state, { payload }) => [...state, payload],
+//   [deleteContact]: (state, { payload }) =>
+//     state.filter(({ id }) => id !== payload),
+// });
+
+// const filterReducer = createReducer('', {
+//   [changeFilter]: (_, { payload }) => payload,
+// });
+
+// const contactsReducer = combineReducers({
+//   items: itemsReducer,
+//   filter: filterReducer,
+// });
+
+export default contactsReducer;
